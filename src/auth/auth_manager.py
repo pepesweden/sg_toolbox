e6cd5f961169fc5858bd0c6bb7290c0f44b59878
pepesweden <warme.patrick@gmail.com>
2025-10-06 20:52:50 +0200

    SGT-124: added methods in auth class for auth, usercreation and admin user

--- src/auth/auth_manager.py
+++ src/auth/auth_manager.py
@@ -5,6 +5,7 @@ class AuthManager:
     # saves database URL to variable
     def __init__(self, database_url):
         self.database_url = database_url
+        self.create_default_admin()
 
     # Takes dependency injection and inserts data into postgres
     def create_user(self, username, password, email=None):
@@ -28,7 +29,7 @@ class AuthManager:
         finally:
             conn.close() 
 
-    #Veridy login
+    #Verify login, compare password hashes
     def authenticate(self, username, password):
         # 1. Connecta till databas
         conn = psycopg2.connect(self.database_url)
@@ -61,8 +62,42 @@ class AuthManager:
             # 7. Stäng connection
             conn.close()
 
+    def get_user(self, username):
+        conn = psycopg2.connect(self.database_url)
+        try:
+            cursor = conn.cursor()
+            cursor.execute(
+                "SELECT username, email, created_at FROM users WHERE username = %s",
+                (username,)
+            )
+            result = cursor.fetchone()
+            
+            if result is None:
+                return None
+            
+            # result är tuple: (username, email, created_at)
+            from auth.models import User
+            return User(
+                username=result[0],
+                email=result[1],
+                created_at=result[2]
+            )
+        finally:
+            conn.close()   
+
+    #Create admin user when auth manager initialises (see __Init__)    
+    def create_default_admin(self):
+        import os
+
+        #Loads variable from .env
+        admin_password = os.getenv('ADMIN_PASSWORD', 'admin123')
     
+
+        if self.get_user('admin'):
+            return  # Admin finns redan
     
+        self.create_user('admin', admin_password, 'admin@salesgroup.se')
+        print("✅ Admin-användare skapad")
      
 
 
